4c58c3caa539da6f466b57ca03b9db774f91f84d
amin havasi <pau.ahq@gmail.com>
2020-08-03 23:40:03 +0430

    redis

--- server.js
+++ server.js
@@ -6,6 +6,28 @@ const redis_port = 6379;
 
 const client = redis.createClient(redis_port);
 const app = express();
+
+function setResponse(username, repose) {
+    return `<h2>${username} has ${repose} Github repose </h2>`;
+}
+app.use(express.json());
+app.get('/:username', async (req, res) => {
+    try {
+        const { username } = req.params;
+        const response = await fetch(`http://api.github.com/users/${username}`);
+        const data = await response.json();
+
+        const repos = data.public_repos;
+        await client.set(username, repos, (err) => {
+            if (err) console.log('err');
+        });
+        res.send(setResponse(username, repos));
+    } catch (err) {
+        console.log(err);
+        res.send(err);
+    }
+});
+
 app.listen(port, () => {
     console.log(`server is running on port ${port}`);
 });
