872eb9601a02bd33dd54b4ab9e8573151919325d
amin havasi <pau.ahq@gmail.com>
2020-08-07 21:19:16 +0430

    register validator

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,4 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
+const { registerValidator } = require('./../validator/auth');
+
+router.get('/', (req, res) => {
+    res.send('amin havasi');
+});
+
+router.post('/register', async (req, res) => {
+    const { error } = await registerValidator(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+    try {
+    } catch (err) {}
+});
+
 module.exports = router;
---
+++ src/validator/auth.js
@@ -0,0 +1,13 @@
+const Joi = require('joi');
+const registerValidator = (user) => {
+    const schema = Joi.object({
+        username: Joi.string().min(3).max(255).required(),
+        email: Joi.string().min(3).max(255).email().required(),
+        password: Joi.string().min(8).max(1024).required(),
+    });
+    return schema.validate(user);
+};
+
+module.exports = {
+    registerValidator,
+};
